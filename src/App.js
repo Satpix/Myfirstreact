00f7459df82535324dec1e989888943a4c3f4a81
Satpix <kamash19@gmail.com>
2020-02-27 13:09:06 +0300

    sa

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sadnes from './Sad';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +23,37 @@ function App() {
     </div>
   );
 }
+*/
+let Header=()=>{
+    return (
+        <div>
+        <div>   <HTagsss />
+        <Tegs />
+        </div>
+        </div>
 
-export default App;
+    )
+}
+let Tegs=()=>{
+    return(
+        <div className="App">
+        <ul>
+        <li>
+        Body</li>
+        <li>qw</li>
+        </ul>
+        </div>
+    );
+}
+let HTagsss=()=>{
+    return (
+        <div className="App">
+        <ul>
+        <li>
+        Head of the SPA</li>
+        <li>!!!</li>
+        </ul>
+        </div>
+);
+}
+export default Header;
